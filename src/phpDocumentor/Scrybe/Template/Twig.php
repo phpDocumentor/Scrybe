3b95a4cebe3f68b8b20b2f22adafb13e058b1126
Mike van Riel <mike.vanriel@naenius.com>
2012-06-20 22:22:38 +0200

    Added DocBlocks to the Twig Template class

--- src/phpDocumentor/Scrybe/Template/Twig.php
+++ src/phpDocumentor/Scrybe/Template/Twig.php
@@ -12,11 +12,21 @@
 
 namespace phpDocumentor\Scrybe\Template;
 
+/**
+ * Template class to use Twig to generate templates.
+ *
+ * @author Mike van Riel <mike.vanriel@naenius.com>
+ */
 class Twig implements TemplateInterface
 {
-    protected $extension = 'html';
-    protected $name = 'default';
+    /** @var string The base location for templates */
     protected $path = '';
+
+    /** @var string The name of the containing template folder */
+    protected $name = 'default';
+
+    /** @var string The extension used to select the correct template file */
+    protected $extension = 'html';
 
     /**
      * Constructs the twig template and sets the default values.
@@ -26,26 +36,116 @@ class Twig implements TemplateInterface
         $this->path = realpath(__DIR__ . '/../../../../data/templates/');
     }
 
+    /**
+     * Sets the name for this template.
+     *
+     * @param string $name A template name that may be composed of alphanumeric
+     *     characters, underscores and/or hyphens.
+     *
+     * @throws \InvalidArgumentException if the name does not match the
+     *   prescribed format.
+     *
+     * @return void
+     */
     public function setName($name)
     {
+        if (!preg_match('/^[0-9a-zA-Z\-\_]$/', $name)) {
+            throw new \InvalidArgumentException(
+                'A template name may only be composed of alphanumeric '
+                .'characters, underscores or hyphens.'
+            );
+        }
+
         $this->name = $name;
     }
 
+    /**
+     * Returns the name of this template.
+     *
+     * See setName() for a specification of the format.
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Sets the base path where the templates are stored.
+     *
+     * @param string $path
+     *
+     * @return void
+     */
     public function setPath($path)
     {
         $this->path = $path;
     }
 
+    /**
+     * Returns the base path where the templates are stored.
+     *
+     * @return string
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * Sets the file extension used to determine the template filename.
+     *
+     * The file extension of the destination format needs to be set. This is
+     * used to retrieve the correct template.
+     *
+     * @param string $extension an extension (thus only containing alphanumeric
+     *     characters and be between 2 and 4 characters in size).
+     *
+     * @throws \InvalidArgumentException if the extension does not match the
+     *     validation restrictions mentioned above.
+     *
+     * @return void
+     */
+    public function setExtension($extension)
+    {
+        if (!preg_match('/^[a-zA-Z0-9]{2,4}$/', $extension)) {
+            throw new \InvalidArgumentException(
+                'Extension should be only be composed of alphanumeric characters'
+                .' and should be at least 2 but no more than 4 characters'
+            );
+        }
+        $this->extension = $extension;
+    }
+
+    /**
+     * Returns the extension of the destination file extension.
+     *
+     * @see setExtension for more information and the format of the extension.
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+
+    /**
+     * Applies the relevant template upon the given content.
+     *
+     * This method takes the combines the template with the given contents and
+     * generates a final piece of text from that.
+     *
+     * The user may add additional options that are set as parameters in the
+     * template.
+     *
+     * @param string   $contents
+     * @param string[] $options
+     *
+     * @see getTemplateFileName() how the filename is assembled
+     *
+     * @return string
+     */
     public function decorate($contents, array $options = array())
     {
         return $this->getTwigEnvironment()->render(
@@ -54,6 +154,21 @@ class Twig implements TemplateInterface
         );
     }
 
+    /**
+     * Returns the filename for the template.
+     *
+     * The filename is composed of the following components:
+     *
+     * - the template base folder
+     * - the template's name
+     * - a path separator
+     * - the literal 'layout' combined with the extension
+     * - and as final extension the literal '.twig'
+     *
+     * @throws \DomainException if the template does not exist.
+     *
+     * @return string
+     */
     protected function getTemplateFilename()
     {
         $filename = $this->name.'/layout.' . $this->extension . '.twig';
@@ -68,6 +183,17 @@ class Twig implements TemplateInterface
         return $filename;
     }
 
+    /**
+     * Constructs and returns the twig environment.
+     *
+     * This uses the path as defined with this class to instantiate a new
+     * Environment and disables the escaping mechanism since we use it to
+     * generate HTML; even embedded.
+     *
+     * @see $path for the template base path.
+     *
+     * @return \Twig_Environment
+     */
     protected function getTwigEnvironment()
     {
         $twig = new \Twig_Environment(new \Twig_Loader_Filesystem($this->path));
@@ -77,15 +203,5 @@ class Twig implements TemplateInterface
         $twig->removeExtension('escaper');
 
         return $twig;
-    }
-
-    public function setExtension($extension)
-    {
-        $this->extension = $extension;
-    }
-
-    public function getExtension()
-    {
-        return $this->extension;
     }
 }
